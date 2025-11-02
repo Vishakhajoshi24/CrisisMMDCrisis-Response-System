303075d33aa18f9074184e0de24045372af5ebf2
Vishakhajoshi24 <149557922+Vishakhajoshi24@users.noreply.github.com>
2025-11-03 01:48:24 +0530

    Update models.py

--- models.py
+++ models.py
@@ -6,28 +6,19 @@ from torchvision import models
 class LateFusionModel(nn.Module):
     def __init__(self):
         super().__init__()
-
-        # TEXT ENCODER (DistilBERT)
+        
+        # Text
         self.text_model = DistilBertModel.from_pretrained("distilbert-base-uncased")
-        self.text_fc = nn.Linear(768, 128)
+        self.text_fc = nn.Linear(768, 2)
 
-        # IMAGE ENCODER (ResNet18)
+        # Image
         resnet = models.resnet18(weights=models.ResNet18_Weights.DEFAULT)
-        resnet.fc = nn.Linear(512, 128)
+        resnet.fc = nn.Linear(512, 2)
         self.image_model = resnet
 
-        # BINARY CLASSIFIER
-        self.classifier = nn.Linear(256, 2)   # Crisis / Not Crisis
-
-    def forward(self, input_ids, attention_mask, images):
-        text_feat = self.text_model(
-            input_ids=input_ids,
-            attention_mask=attention_mask
-        ).last_hidden_state[:, 0, :]
-        text_feat = self.text_fc(text_feat)
-
-        img_feat = self.image_model(images)
-
-        fused = torch.cat([text_feat, img_feat], dim=1)
-
-        return self.classifier(fused)
+    def forward(self, input_ids, attention_mask, image):
+        text_feat = self.text_model(input_ids=input_ids, attention_mask=attention_mask).last_hidden_state[:,0,:]
+        text_logits = self.text_fc(text_feat)
+        image_logits = self.image_model(image)
+        
+        return (text_logits + image_logits) / 2
